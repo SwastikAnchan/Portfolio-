bd8da562632f8250b8cfa014f92b155e8fe55323
Swastik Anchan <158566827+SwastikAnchan@users.noreply.github.com>
2024-12-26 12:40:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Typed('.typing', {
+      strings: ["Swastik Anchan", "a Web Developer", "a Software Intern"],
+      typeSpeed: 80,
+      backSpeed: 50,
+      loop: true
+  });
+});
